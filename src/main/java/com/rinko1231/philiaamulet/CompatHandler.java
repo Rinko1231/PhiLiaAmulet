2986f904c1079d9b463eaff647c1084da62472ce
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2025-07-27 10:00:58 +0800

    1.0.6

--- src/main/java/com/rinko1231/philiaamulet/CompatHandler.java
+++ src/main/java/com/rinko1231/philiaamulet/CompatHandler.java
@@ -1,17 +1,19 @@
 package com.rinko1231.philiaamulet;
 
-import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
 
 public class CompatHandler {
     private static CompatHandler instance;
     public final boolean ironMagicLoaded;
-
     public static CompatHandler getInstance() {
         if (instance == null) instance = new CompatHandler();
         return instance;
     }
-
     public CompatHandler() {
-        this.ironMagicLoaded = ModList.get().isLoaded("ironsspellbooks");
+        ironMagicLoaded = getViaCfgAndClass();
     }
+    private boolean getViaCfgAndClass() {
+        return FMLLoader.getLoadingModList().getModFileById("irons_spellbooks") !=null;
+    }
+
 }
